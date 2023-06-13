b50da59df42a574bbc0cf57723b870db4af3a42c
trezcx <20paltayev08@gmail.com>
2023-06-13 12:16:00 +0500

    codewars

---
+++ cw1.py
@@ -0,0 +1,7 @@
+def simple_multiplication(number):
+    if number % 2 == 0:
+        print(number*8)
+    else:
+        print(number*9)
+
+simple_multiplication(number = int(input(": ")))
---
+++ cw2.py
@@ -0,0 +1,5 @@
+def double(a):
+    for i in a:
+        print(i*2)
+
+double(a = [1, 2, 3, 4, 5])
